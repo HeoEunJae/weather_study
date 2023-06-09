445caa9ca95ef946863e9efc25501a3a3aaafd8b
dmswo159 <dmswo159@gmail.com>
2023-06-09 14:42:36 +0900

    ㅇㄴㅇ

--- lib/Screens/weather_screen.dart
+++ lib/Screens/weather_screen.dart
@@ -3,7 +3,6 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:timer_builder/timer_builder.dart';
 import 'package:intl/intl.dart';
-import 'package:test_test/model/model.dart';
 
 class WeatherScreen extends StatefulWidget {
   const WeatherScreen(
@@ -15,10 +14,8 @@ class WeatherScreen extends StatefulWidget {
 }
 
 class _WeatherScreenState extends State<WeatherScreen> {
-  Model model = Model();
   String? cityname;
   int? temp;
-  Widget? icon;
   var date = DateTime.now();
   @override
   void initState() {
@@ -30,9 +27,7 @@ class _WeatherScreenState extends State<WeatherScreen> {
   void updateData(dynamic weatherData) {
     double temp2 = weatherData['main']['temp'];
     temp = temp2.round();
-    int condition = weatherData['weather'][0]['id'];
     cityname = weatherData['name'];
-    icon = model.getWeatherIcon(condition);
   }
 
   String getSystemTime() {
@@ -141,11 +136,7 @@ class _WeatherScreenState extends State<WeatherScreen> {
                             ),
                             Row(
                               children: [
-                                // SvgPicture.asset('svg/climacon-sun.svg'),
-                                icon!,
-                                SizedBox(
-                                  width: 10,
-                                ),
+                                SvgPicture.asset('svg/climacon-sun.svg'),
                                 Text(
                                   'clear sky',
                                   style: GoogleFonts.lato(
